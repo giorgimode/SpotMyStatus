606e47b1a27282cab2048d17d1668d6d88b7c545
giorgimode <modegiorgi@gmail.com>
2020-09-26 16:37:17 +0200

    inital commit: spotify auth token

---
+++ src/main/java/com/giorgimode/SpotMyStatus/spotify/SpotifyAgent.java
@@ -0,0 +1,54 @@
+package com.giorgimode.SpotMyStatus.spotify;
+
+import static com.giorgimode.SpotMyStatus.spotify.SpotifyScopes.USER_CURRENTLY_PLAYING;
+import static com.giorgimode.SpotMyStatus.spotify.SpotifyScopes.USER_TOP_READ;
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.enums.ModelObjectType;
+import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import com.wrapper.spotify.model_objects.miscellaneous.CurrentlyPlaying;
+import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
+import com.wrapper.spotify.model_objects.specification.Track;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+import com.wrapper.spotify.requests.data.player.GetUsersCurrentlyPlayingTrackRequest;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpotifyAgent {
+
+    @Autowired
+    private SpotifyApi spotifyApi;
+
+    @SneakyThrows
+    public void updateAuthToken(String code) {
+        AuthorizationCodeRequest authorCodeRequest = spotifyApi.authorizationCode(code).build();
+        AuthorizationCodeCredentials codeCredentials = authorCodeRequest.execute();
+        spotifyApi.setAccessToken(codeCredentials.getAccessToken());
+        spotifyApi.setRefreshToken(codeCredentials.getRefreshToken());
+    }
+
+    public URI requestAuthorization() {
+        AuthorizationCodeUriRequest authCodeUriRequest = spotifyApi.authorizationCodeUri()
+                                                                   .scope(USER_CURRENTLY_PLAYING + " " + USER_TOP_READ)
+                                                                   .build();
+        return authCodeUriRequest.execute();
+    }
+
+
+    @SneakyThrows
+    public void test() {
+        GetUsersCurrentlyPlayingTrackRequest playingTrackRequest = spotifyApi.getUsersCurrentlyPlayingTrack().build();
+        CurrentlyPlaying currentlyPlaying = playingTrackRequest.execute();
+        System.out.println("*********Track is currently playing: " + currentlyPlaying.getIs_playing());
+        if (currentlyPlaying.getItem().getType() == ModelObjectType.TRACK) {
+            Track track = (Track) currentlyPlaying.getItem();
+            String artists = Arrays.stream(track.getArtists()).map(ArtistSimplified::getName).collect(Collectors.joining(", "));
+            System.out.println("*********Track: " + artists + " - " + track.getName());
+        }
+    }
+}
---
+++ src/test/java/com/giorgimode/SpotMyStatus/SpotMyStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giorgimode.SpotMyStatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotMyStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
