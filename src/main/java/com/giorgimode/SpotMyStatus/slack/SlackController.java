1e9380537c62bb5dfdb47bc061b31b91890c7efc
gio <giorgi@gini.net>
2020-09-27 12:04:29 +0200

    SMS-1 cleaned up redundant endpoints

--- src/main/java/com/giorgimode/SpotMyStatus/slack/SlackController.java
+++ src/main/java/com/giorgimode/SpotMyStatus/slack/SlackController.java
@@ -1,6 +1,5 @@
 package com.giorgimode.SpotMyStatus.slack;
 
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,24 +11,10 @@ public class SlackController {
     @Autowired
     public SlackAgent slackAgent;
 
-    @RequestMapping("/test2")
-    public String currentTrack() {
-        return slackAgent.updateStatus();
-    }
-
-
-    @RequestMapping("/start2")
-    public void triggerApiCall(HttpServletResponse httpServletResponse) {
-        String authorization = slackAgent.requestAuthorization();
-        System.out.println("****Redirect***" + authorization);
-        httpServletResponse.setHeader("Location", authorization);
-        httpServletResponse.setStatus(302);
-    }
-
     @RequestMapping("/redirect2")
     public String redirectEndpoint(@RequestParam(value = "code") String slackCode) {
         System.out.println("****Slack Code: x" + slackCode);
         slackAgent.updateAuthToken(slackCode);
-        return "Say hello to my little friend!";
+        return slackAgent.updateStatus();
     }
 }
