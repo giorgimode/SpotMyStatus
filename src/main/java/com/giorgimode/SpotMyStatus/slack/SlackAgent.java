d58c1917409b6c5e0c4c6ec0220a3711112dd93c
gio <giorgi@gini.net>
2020-09-28 21:44:10 +0200

    SMS-7 update status only when user is online and status hasn't been manually changed

--- src/main/java/com/giorgimode/SpotMyStatus/slack/SlackAgent.java
+++ src/main/java/com/giorgimode/SpotMyStatus/slack/SlackAgent.java
@@ -1,5 +1,7 @@
 package com.giorgimode.SpotMyStatus.slack;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import com.giorgimode.SpotMyStatus.model.SlackToken;
 import com.giorgimode.SpotMyStatus.model.SpotifyCurrentTrackResponse;
 import com.giorgimode.SpotMyStatus.persistence.User;
@@ -52,12 +54,11 @@ public class SlackAgent {
                                           .getBody();
 
         String userString = RestHelper.builder()
-                                 .withBaseUrl("https://slack.com/api/users.info")
-                                 .withBearer(slackToken.getAccessToken())
-                                 .withContentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                                 .withQueryParam("user", slackToken.getId())
-                                 .post(restTemplate, String.class)
-                                 .getBody();
+                                      .withBaseUrl("https://slack.com/api/users.info")
+                                      .withBearer(slackToken.getAccessToken())
+                                      .withContentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                                      .withQueryParam("user", slackToken.getId())
+                                      .getBody(restTemplate, String.class);
 
         log.debug(userString);
         Integer timezoneOffsetInSeconds = JsonPath.read(userString, "$.user.tz_offset");
@@ -74,12 +75,14 @@ public class SlackAgent {
 
     public void updateAndPersistStatus(User user, SpotifyCurrentTrackResponse currentTrack) {
         long expiringIn = currentTrack.getDurationMs() - currentTrack.getProgressMs();
-        String slackStatus = currentTrack.getArtists() + " - " + currentTrack.getSongTitle();
-        log.info("Track: {} expiring in {}", slackStatus, expiringIn);
-        SlackStatusPayload statusPayload = new SlackStatusPayload(slackStatus, ":headphones:", expiringIn);
-        updateStatus(user, statusPayload);
-
-        user.setSlackStatus(slackStatus);
+        String newSlackStatus = currentTrack.getArtists() + " - " + currentTrack.getSongTitle();
+        SlackStatusPayload statusPayload = new SlackStatusPayload(newSlackStatus, ":headphones:", expiringIn);
+        // todo fails to update if user manually changes the status and then cleans it
+        if (!newSlackStatus.equalsIgnoreCase(user.getSlackStatus())) {
+            log.info("Track: {} expiring in {}", newSlackStatus, expiringIn);
+            user.setSlackStatus(newSlackStatus);
+            updateStatus(user, statusPayload);
+        }
         user.setUpdatedAt(LocalDateTime.now());
         userRepository.save(user);
     }
@@ -99,4 +102,33 @@ public class SlackAgent {
         updateStatus(user, statusPayload);
     }
 
+    public boolean isUserOnline(User user) {
+        String userPresenceResponse = RestHelper.builder()
+                                                .withBaseUrl("https://slack.com/api/users.getPresence")
+                                                .withBearer(user.getSlackAccessToken())
+                                                .getBody(restTemplate, String.class);
+
+        String usersPresence = JsonPath.read(userPresenceResponse, "$.presence");
+        boolean isUserActive = "active".equalsIgnoreCase(usersPresence);
+//        if (!isUserActive) { todo clean only once
+//            log.info("User {} is away.", user.getId());
+//            cleanStatus(user);
+//        }
+        return isUserActive;
+    }
+
+    public boolean statusHasNotBeenManuallyChanged(User user) {
+        String userProfile = RestHelper.builder()
+                                       .withBaseUrl("https://slack.com/api/users.profile.get")
+                                       .withBearer(user.getSlackAccessToken())
+                                       .getBody(restTemplate, String.class);
+
+        String statusText = JsonPath.read(userProfile, "$.profile.status_text");
+        boolean statusHasNotBeenManuallyChanged = isBlank(statusText) || isNotBlank(user.getSlackStatus()) &&
+            user.getSlackStatus().equalsIgnoreCase(statusText);
+        if (!statusHasNotBeenManuallyChanged) {
+            log.info("Status for user {} has been manually changed. Skipping the update.", user.getId());
+        }
+        return statusHasNotBeenManuallyChanged;
+    }
 }
