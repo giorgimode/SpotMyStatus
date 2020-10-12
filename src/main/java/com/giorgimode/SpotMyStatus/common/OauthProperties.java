fd10df2edf368fcd9b0357f23822ac3c8600cc88
gio <giorgi@gini.net>
2020-10-12 22:15:17 +0200

    SMS-17 added command support

--- src/main/java/com/giorgimode/SpotMyStatus/common/OauthProperties.java
+++ src/main/java/com/giorgimode/SpotMyStatus/common/OauthProperties.java
@@ -1,11 +1,17 @@
 package com.giorgimode.SpotMyStatus.common;
 
+import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class OauthProperties {
 
+    @NotEmpty
     private String clientId;
+
+    @NotEmpty
     private String clientSecret;
+
     private String botToken;
+    private String signingSecret;
 }
