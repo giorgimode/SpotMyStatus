056d8cd73674bcc663f4cebbed667dfed90831ef
gio <giorgi@gini.net>
2020-11-08 18:06:37 +0100

    SMS-28 updated bot scopes

--- backend/src/main/java/com/giorgimode/SpotMyStatus/common/SpotConstants.java
+++ backend/src/main/java/com/giorgimode/SpotMyStatus/common/SpotConstants.java
@@ -7,4 +7,5 @@ public class SpotConstants {
     public static final String SLACK_REDIRECT_PATH = "/slack/redirect";
     public static final String SPOTIFY_REDIRECT_PATH = "/spotify/redirect";
     public static final List<String> SLACK_PROFILE_SCOPES = List.of("users:read", "users.profile:read", "users.profile:write");
+    public static final List<String> SLACK_BOT_SCOPES = List.of("chat:write", "commands");
 }
