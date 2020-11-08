c7b869680be67eb969006cea7d2d8ab85bb5bb52
gio <giorgi@gini.net>
2020-11-08 15:34:07 +0100

    SMS-26 add scope when user signs up after revoked token

--- backend/src/main/java/com/giorgimode/SpotMyStatus/common/SpotConstants.java
+++ backend/src/main/java/com/giorgimode/SpotMyStatus/common/SpotConstants.java
@@ -1,9 +1,10 @@
 package com.giorgimode.SpotMyStatus.common;
 
+import java.util.List;
+
 public class SpotConstants {
 
     public static final String SLACK_REDIRECT_PATH = "/slack/redirect";
     public static final String SPOTIFY_REDIRECT_PATH = "/spotify/redirect";
-    public static final String SLACK_PROFILE_READ_SCOPE = "users.profile:read";
-    public static final String SLACK_PROFILE_WRITE_SCOPE = "users.profile:write";
+    public static final List<String> SLACK_PROFILE_SCOPES = List.of("users:read", "users.profile:read", "users.profile:write");
 }
