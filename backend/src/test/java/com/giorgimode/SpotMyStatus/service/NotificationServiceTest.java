54f7a237fb6fcf472296e06065e5f5493214f0d6
giorgimode <modegiorgi@gmail.com>
2020-11-21 20:46:54 +0100

    fixed test

--- backend/src/test/java/com/giorgimode/SpotMyStatus/service/NotificationServiceTest.java
+++ backend/src/test/java/com/giorgimode/SpotMyStatus/service/NotificationServiceTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,7 +38,7 @@ class NotificationServiceTest {
 
     @BeforeEach
     void setUp() {
-        ReflectionTestUtils.setField(notificationService, "slackUri", "http://localhost:8080");
+
     }
 
     @Test
