6f95b7ad326663c9be184ee6a82877120ba86461
giorgimode <modegiorgi@gmail.com>
2020-12-24 18:52:12 +0100

    SMS-55 move all controller handlers under /api

--- backend/src/main/java/com/giorgimode/spotmystatus/controller/AuthorizationController.java
+++ backend/src/main/java/com/giorgimode/spotmystatus/controller/AuthorizationController.java
@@ -9,11 +9,13 @@ import java.io.IOException;
 import java.util.UUID;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
 @Slf4j
 public class AuthorizationController {
 
@@ -27,7 +29,7 @@ public class AuthorizationController {
         this.slackClient = slackClient;
     }
 
-    @RequestMapping("/start")
+    @GetMapping("/start")
     public void startNewUser(HttpServletResponse httpServletResponse) {
         log.info("Starting authorization for a new user...");
         String authorization = slackClient.requestAuthorization();
--- backend/src/main/java/com/giorgimode/spotmystatus/controller/UserInteractionController.java
+++ backend/src/main/java/com/giorgimode/spotmystatus/controller/UserInteractionController.java
@@ -17,10 +17,12 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
 @Slf4j
 public class UserInteractionController {
 
